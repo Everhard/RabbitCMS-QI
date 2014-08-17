9ee7a8ee26325babccb2999790b817f795cc3b33
Andrey Dorokhov <andrew@dorohoff.net>
2014-08-17 03:00:21 +0300

    Added basic html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Quick Install for RabbitCMS</title>
+  </head>
+  <body>
+	<h1>Quick install for RabbitCMS</h1>
+	<form method="post">
+		<input type="submit" name="install" value="Install" />
+	</form>
+  </body>
+</html>
